dea677fdcc6765dcd824c32159f4a2154041eda0
姜涛 <jiangtao2@streamax.com>
2022-01-12 09:25:26 +0800

    新增两个数据的基础配置文件，主要用于人脸对齐与眼部对齐。

---
+++ configs/_base_/datasets/dms.py
@@ -0,0 +1,77 @@
+dataset_info = dict(
+    dataset_name='DMS',
+    paper_info=dict(
+        author='jiangao',
+        title='streamax dms dataset',
+        container='Image and vision computing',
+        year='2021',
+        homepage='streamax.com',
+    ),
+    keypoint_info={
+        0:
+        dict(
+            name='kpt-0', id=0, color=[255, 255, 255], type='', swap='kpt-12'),
+        1:
+        dict(
+            name='kpt-1', id=1, color=[255, 255, 255], type='', swap='kpt-9'),
+        2:
+        dict(
+            name='kpt-2', id=2, color=[255, 255, 255], type='', swap='kpt-8'),
+        3:
+        dict(
+            name='kpt-3', id=3, color=[255, 255, 255], type='', swap='kpt-7'),
+        4:
+        dict(
+            name='kpt-4', id=4, color=[255, 255, 255], type='', swap='kpt-10'),
+        5:
+        dict(
+            name='kpt-5', id=5, color=[255, 255, 255], type='', swap='kpt-11'),
+        6:
+        dict(
+            name='kpt-6', id=6, color=[255, 255, 255], type='', swap=''),
+        7:
+        dict(
+            name='kpt-7', id=7, color=[255, 255, 255], type='', swap='kpt-3'),
+        8:
+        dict(
+            name='kpt-8', id=8, color=[255, 255, 255], type='', swap='kpt-2'),
+        9:
+        dict(
+            name='kpt-9', id=9, color=[255, 255, 255], type='', swap='kpt-1'),
+        10:
+        dict(
+            name='kpt-10', id=10, color=[255, 255, 255], type='',swap='kpt-4'),
+        11:
+        dict(
+            name='kpt-11', id=11, color=[255, 255, 255], type='',swap='kpt-5'),
+        12:
+        dict(
+            name='kpt-12', id=12, color=[255, 255, 255], type='',swap='kpt-0'),
+        13:
+        dict(
+            name='kpt-13', id=13, color=[255, 255, 255], type='',swap=''),
+        14:
+        dict(
+            name='kpt-14', id=14, color=[255, 255, 255], type='',swap=''),
+        15:
+        dict(
+            name='kpt-15', id=15, color=[255, 255, 255], type='',swap='kpt-17'),
+        16:
+        dict(
+            name='kpt-16', id=16, color=[255, 255, 255], type='',swap=''),
+        17:
+        dict(
+            name='kpt-17', id=17, color=[255, 255, 255], type='',swap='kpt-15'),
+        18:
+        dict(
+            name='kpt-18', id=18, color=[255, 255, 255], type='',swap=''),
+        19:
+        dict(
+            name='kpt-19', id=19, color=[255, 255, 255], type='',swap=''),
+        20:
+        dict(
+            name='kpt-20', id=20, color=[255, 255, 255], type='',swap='')
+    },
+    skeleton_info={},
+    joint_weights=[1.] * 21,
+    sigmas=[])
---
+++ configs/_base_/datasets/gaze.py
@@ -0,0 +1,65 @@
+dataset_info = dict(
+    dataset_name='gaze',
+    paper_info=dict(
+        author='jiangao',
+        title='streamax gaze dataset',
+        container='Image and vision computing',
+        year='2021',
+        homepage='streamax.com',
+    ),
+    keypoint_info={
+        0:
+        dict(
+            name='kpt-0', id=0, color=[255, 255, 255], type='', swap='kpt-4'),
+        1:
+        dict(
+            name='kpt-1', id=1, color=[255, 255, 255], type='', swap='kpt-3'),
+        2:
+        dict(
+            name='kpt-2', id=2, color=[255, 255, 255], type='', swap=''),
+        3:
+        dict(
+            name='kpt-3', id=3, color=[255, 255, 255], type='', swap='kpt-1'),
+        4:
+        dict(
+            name='kpt-4', id=4, color=[255, 255, 255], type='', swap='kpt-0'),
+        5:
+        dict(
+            name='kpt-5', id=5, color=[255, 255, 255], type='', swap='kpt-7'),
+        6:
+        dict(
+            name='kpt-6', id=6, color=[255, 255, 255], type='', swap=''),
+        7:
+        dict(
+            name='kpt-7', id=7, color=[255, 255, 255], type='', swap='kpt-5'),
+        8:
+        dict(
+            name='kpt-8', id=8, color=[255, 255, 255], type='', swap='kpt-12'),
+        9:
+        dict(
+            name='kpt-9', id=9, color=[255, 255, 255], type='', swap='kpt-11'),
+        10:
+        dict(
+            name='kpt-10', id=10, color=[255, 255, 255], type='',swap=''),
+        11:
+        dict(
+            name='kpt-11', id=11, color=[255, 255, 255], type='',swap='kpt-9'),
+        12:
+        dict(
+            name='kpt-12', id=12, color=[255, 255, 255], type='',swap='kpt-8'),
+        13:
+        dict(
+            name='kpt-13', id=13, color=[255, 255, 255], type='',swap='kpt-15'),
+        14:
+        dict(
+            name='kpt-14', id=14, color=[255, 255, 255], type='',swap=''),
+        15:
+        dict(
+            name='kpt-15', id=15, color=[255, 255, 255], type='',swap='kpt-13'),
+        16:
+        dict(
+            name='kpt-16', id=16, color=[255, 255, 255], type='',swap='')
+    },
+    skeleton_info={},
+    joint_weights=[1.] * 17,
+    sigmas=[])
